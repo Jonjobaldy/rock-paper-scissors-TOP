45df2dac6c37b3fe6ee9dc329bff0d1b60a73530
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-09-16 22:30:12 +0100

    completed lesson, announcing winner after score reaches 5

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
+let userScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll("button");
+
 function getComputerChoice () {
     let guess = Math.random();
     if (guess <=0.33){
@@ -14,72 +18,56 @@ function playRound(playerSelection) {
 
     if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
         resultsDiv.textContent = "You win!" + " " + playerSelection + " " + "beats" + " " + computerSelection;
-        return 0;
+        userScore += 1;
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
         resultsDiv.textContent = "You lose!" + " " + playerSelection + " " + "loses to" + " " + computerSelection;
-        return 1;
+        computerScore += 1;
     } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
         resultsDiv.textContent = "Draw!" + " " + playerSelection + " " + "matches" + " " + computerSelection;
-        return 2;
     } else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
         resultsDiv.textContent = "Draw!" + " " + playerSelection + " " + "matches" + " " + computerSelection;
-        return 2;
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         resultsDiv.textContent = "You win!" + " " + playerSelection + " " + "beats" + " " + computerSelection;
-        return 0;
+        userScore += 1;
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
         resultsDiv.textContent = "You lose!" + " " + playerSelection + " " + "loses to" + " " + computerSelection;
-        return 1;
+        computerScore += 1;
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
         resultsDiv.textContent = "You lose!" + " " + playerSelection + " " + "loses to" + " " + computerSelection;
-        return 1;
+        computerScore += 1;
     } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
         resultsDiv.textContent = "Draw!" + " " + playerSelection + " " + "matches" + " " + computerSelection;
-        return 2;
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
         resultsDiv.textContent = "You Win!" + " " + playerSelection + " " + "beats" + " " + computerSelection;
-        return 0;
+        userScore +=1;
     } else {
         resultsDiv.textContent = "Error! Please Try again.";
     }
+    const score = document.createElement("p");
+    score.textContent = "User score:" + userScore + " " + "Computer score:" + computerScore;
+    resultsDiv.appendChild(score);
 }
 
+function checkWinner(userScore, computerScore){
+    if(userScore === 5){
+        const endText = document.createElement("h2");
+        endText.innerText = `You won ${userScore} to ${computerScore}`;
+        resultsDiv.appendChild(endText);
+    } else if(computerScore === 5){
+        const endText = document.createElement("h2");
+        endText.innerText = `You lost ${userScore} to ${computerScore}`;
+        resultsDiv.appendChild(endText);
+    }
 
-const buttons = document.querySelectorAll("button");
+    }
+
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
+    button.addEventListener("click", () => {
         playRound(button.id);
-    ;
-  });
-});
+        checkWinner(userScore, computerScore);
+    });
+    });
 
 const resultsDiv = document.querySelector("div");
 
-let userScore = 0;
-let computerScore = 0;
-
-while (userScore <3 && computerScore <3) {
-
-        let winner = playRound(playerSelection,computerSelection);
-        if (winner === 0) {
-            userScore += 1;
-        } else if (winner === 1) {
-            computerScore += 1;
-        } else {
-            continue;
-        }
-        console.log(`Your score : ${userScore}, Computer Score: ${computerScore}`)
-    }
-    
-if (userScore === 3) {
-        alert("You Won!");
-    } else if (computerScore === 3) {
-        alert("You Lost!");
-    } else {
-        alert("Draw!");
-    }
-
-
-
-//playGame();
